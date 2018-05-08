d3eaced0fdcc3e96b0303a5e974f79a047cf9de8
ozdenurucar <ozdenuru@gmail.com>
2018-05-08 18:06:19 +0300

    En son yapilan her sey eklendi.

---
+++ document_classify/document_classify/Documents_Info.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace document_classify
+{
+    class DocumentsInfo
+    {
+        private FileInfo[] files;
+        private string categori;
+        private  Dictionary<string, int> frequencyOfWords = new Dictionary<string, int>();// kelime-->sıklığı şekilde tutulacak olan veriyapısı
+
+        public FileInfo[] Files { get => files; set => files = value; }
+        public string Categori { get => categori; set => categori = value; }
+        public Dictionary<string, int> FrequencyOfWords { get => frequencyOfWords; set => frequencyOfWords = value; }
+
+
+        public void InitFiles()
+        {
+            var file = File.ReadAllText(files[0].FullName);
+            Console.WriteLine(file);
+        }
+    }
+}
