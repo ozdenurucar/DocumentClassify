800b4e255a0eeb7ca0f96ac1537924ba21f6e52c
ozdenurucar <ozdenuru@gmail.com>
2018-05-09 22:56:13 +0300

    Stop wordleri Temizlendi

--- document_classify/document_classify/Documents_Info.cs
+++ document_classify/document_classify/Documents_Info.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 using System.IO;
 using System.Text.RegularExpressions; //noktalama işaretlerini temizlemek için kullanıldı
 using System.Data.OleDb;
@@ -31,22 +32,29 @@ namespace document_classify
                 var replacePunctuation = Regex.Replace(replaceBrackets,@"[^\w,]+", " ");// \u karakterini temizlemek için
                 file = replacePunctuation;
                 file = Regex.Replace(file, @"\p{P}", "");//Noktalama işaretlerini temizleme
-                file=file.Replace(" ", "_");
                 All_texts.Add(file.ToLower());//TÜm karakterleri küçük harfe çevirme
+                All_texts[i]=clear_the_stop_words(All_texts[i]);//stop wrodleri temizle
+                All_texts[i] = All_texts[i].Replace(" ", "_");// boşluğu "_" yap
             }
         }
-        public void Connect_database()
+        public string clear_the_stop_words(string words)
         {
-            OleDbConnection connect = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source=stopwords.accdb");
+            //Stop wordler temizlenirken stop_words_txt doyası okunarak içerisindeki stop wordler alınır. Daha sonra her ham txt doyası için
+            //stop wordleriniçerisinde var mı diye bakılır. Eğer varsa yeni oluşturulan string new_words stringine eklenmez.
+            string[] words_array = words.Split(' ');
+           // List<string> list = new List<string>();//Kelime sayısındaki değişimi görmek için ekledim
+            string new_words="";
+            var stop_words = File.ReadAllLines("stop_words.txt", Encoding.GetEncoding("ISO-8859-9"));
+            for (int i = 0; i < words_array.Length; i++)
+            {
+                if(!stop_words.Contains(words_array[i]))
+                {
+                    new_words+=words_array[i]+" ";
+                    //list.Add(words_array[i]);
+                }
+            }
+            return new_words;
 
-
-
-        }
-        public List<string> clear_the_stop_words(List<string> words)
-        {
-
-
-            return words;
         }
     }
 }
