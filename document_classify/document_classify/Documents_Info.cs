c026280b984d6def7c038bcd40e3e5323b21cb5d
Alperen Çakıroğlu <alperenckroglu204@gmail.com>
2018-05-11 02:06:38 +0300

    Add files via upload

--- document_classify/document_classify/Documents_Info.cs
+++ document_classify/document_classify/Documents_Info.cs
@@ -16,64 +16,52 @@ namespace document_classify
 {
     class DocumentsInfo
     {
-        private FileInfo[] files;
-        private string categori;
-        private Dictionary<string, int> frequencyOfWords = new Dictionary<string, int>();// kelime-->sıklığı şekilde tutulacak olan veriyapısı
-        private List<string> all_texts = new List<string>();
-        private List<string> stop_words = new List<string>();
-        public FileInfo[] Files { get => files; set => files = value; }
-        public string Categori { get => categori; set => categori = value; }
-        public Dictionary<string, int> FrequencyOfWords { get => frequencyOfWords; set => frequencyOfWords = value; }
-        public List<string> All_texts { get => all_texts; set => all_texts = value; }
-        public List<string> Stop_words { get => stop_words; set => stop_words = value; }
+        public FileInfo[] files;
+        public string Categori { get; set; }
+        public Dictionary<string, int> FrequencyOfWords { get; set; } = new Dictionary<string, int>();
+        public List<string> AllTexts { get; set; } = new List<string>();
+        public List<string> StopWords { get; set; } = new List<string>();
+
+        Zemberek zemberek = new Zemberek(new TurkiyeTurkcesi());
 
         public void InitFiles()
         {
-            for (int i = 0; i < files.Length; i++)
+            foreach(var iter in files)
             {
-                var file = File.ReadAllText(files[i].FullName, Encoding.GetEncoding("ISO-8859-9"));
-                var replaceBrackets = Regex.Replace(file,@"\((?'content'[^)]+)\)", match => $", {match.Groups["content"].Value}");// \r\n  gibi karakterleri temizlemek için
-                var replacePunctuation = Regex.Replace(replaceBrackets,@"[^\w,]+", " ");// \u karakterini temizlemek için
+                var file = File.ReadAllText(iter.FullName, Encoding.GetEncoding("ISO-8859-9"));
+                var replaceBrackets = Regex.Replace(file, @"\((?'content'[^)]+)\)", match => $", {match.Groups["content"].Value}");
+                var replacePunctuation = Regex.Replace(replaceBrackets, @"[^\w,]+", " ");// \u karakterini temizlemek için
                 file = replacePunctuation;
                 file = Regex.Replace(file, @"\p{P}", "");//Noktalama işaretlerini temizleme
-                All_texts.Add(file.ToLower());//TÜm karakterleri küçük harfe çevirme
-                All_texts[i]=clear_the_stop_words(All_texts[i]);//stop wrodleri temizle
-                All_texts[i] = All_texts[i].Replace(" ", "_");// boşluğu "_" yap
+                file = file.ToLower().Replace(" ", "_");
+                file = ClearTheStopWords(file);
+                file = deleteSuffixes(file);
+                AllTexts.Add(file);//Tüm karakterleri küçük harfe çevirme
+
             }
         }
-        public string clear_the_stop_words(string words)
+        public string ClearTheStopWords(string text)
         {
             //Stop wordler temizlenirken stop_words_txt doyası okunarak içerisindeki stop wordler alınır. Daha sonra her ham txt doyası için
             //stop wordleriniçerisinde var mı diye bakılır. Eğer varsa yeni oluşturulan string new_words stringine eklenmez.
-            string[] words_array = words.Split(' ');
-            List<string> list = new List<string>();//Kelime sayısındaki değişimi görmek için ekledim
-            string new_words="";
-            List<string> kokler = new List<string>();
-            var stop_words = File.ReadAllLines("stop_words.txt", Encoding.GetEncoding("ISO-8859-9"));
-            for (int i = 0; i < words_array.Length; i++)
+            string[] words = text.Split('_');
+            string result = "";
+            var stopWords = File.ReadAllLines("stop_words.txt", Encoding.GetEncoding("ISO-8859-9"));
+            foreach(var word in words)
             {
-                if(!stop_words.Contains(words_array[i]))
+                if(!stopWords.Contains(word))
                 {
-                    new_words+=words_array[i]+" ";
-                    list.Add(words_array[i]);
+                    result += word + "_";
                 }
             }
-            kokler = find_words_root(list);
-            return new_words;
+            Console.WriteLine(result);
+            return result;
         }
 
-        public List<string> find_words_root(List<string> words)
+        public string deleteSuffixes(string text)
         {
-            List<string> word_roots = new List<string>();
-            Zemberek zemberek = new Zemberek(new TurkiyeTurkcesi());
-            for (int i = 0; i < words.Count(); i++)
-            {
-                if(zemberek.kelimeDenetle(words[i]))//Kelimeyi denetliyor türkçe olup olmamasına göre
-                {
-                    word_roots.Add(zemberek.kelimeCozumle(words[i])[0].kok().icerik());// kelimenin kök bilgisini veriyor
-                }
-            }
-            return word_roots;
+            
+            return "";
         }
     }
 }
