4a319c249e30bf8e9fa9fdd2320cd9e1b13fe42c
ozdenurucar <ozdenuru@gmail.com>
2018-05-10 20:18:15 +0300

    Kök bulma kısmı eklendi

--- document_classify/document_classify/Documents_Info.cs
+++ document_classify/document_classify/Documents_Info.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using System.Data;
 using System.IO;
 using System.Text.RegularExpressions; //noktalama işaretlerini temizlemek için kullanıldı
+using net.zemberek.erisim;
+using net.zemberek.yapi;
+using net.zemberek.tr.yapi;
+using net.zemberek.bilgi;
 
 
 namespace document_classify
@@ -42,19 +46,34 @@ namespace document_classify
             //Stop wordler temizlenirken stop_words_txt doyası okunarak içerisindeki stop wordler alınır. Daha sonra her ham txt doyası için
             //stop wordleriniçerisinde var mı diye bakılır. Eğer varsa yeni oluşturulan string new_words stringine eklenmez.
             string[] words_array = words.Split(' ');
-           // List<string> list = new List<string>();//Kelime sayısındaki değişimi görmek için ekledim
+            List<string> list = new List<string>();//Kelime sayısındaki değişimi görmek için ekledim
             string new_words="";
+            List<string> kokler = new List<string>();
             var stop_words = File.ReadAllLines("stop_words.txt", Encoding.GetEncoding("ISO-8859-9"));
             for (int i = 0; i < words_array.Length; i++)
             {
                 if(!stop_words.Contains(words_array[i]))
                 {
                     new_words+=words_array[i]+" ";
-                    //list.Add(words_array[i]);
+                    list.Add(words_array[i]);
                 }
             }
+            kokler = find_words_root(list);
             return new_words;
+        }
 
+        public List<string> find_words_root(List<string> words)
+        {
+            List<string> word_roots = new List<string>();
+            Zemberek zemberek = new Zemberek(new TurkiyeTurkcesi());
+            for (int i = 0; i < words.Count(); i++)
+            {
+                if(zemberek.kelimeDenetle(words[i]))//Kelimeyi denetliyor türkçe olup olmamasına göre
+                {
+                    word_roots.Add(zemberek.kelimeCozumle(words[i])[0].kok().icerik());// kelimenin kök bilgisini veriyor
+                }
+            }
+            return word_roots;
         }
     }
 }
