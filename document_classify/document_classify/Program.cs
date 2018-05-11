53e9b2fcce7dba784c95d0d9022b18e87bc7c183
alperenckr <alperenckroglu204@gmail.com>
2018-05-11 18:17:57 +0300

    bug fixes

--- document_classify/document_classify/Program.cs
+++ document_classify/document_classify/Program.cs
@@ -13,16 +13,16 @@ namespace document_classify
 
         static List<DocumentsInfo> documents = new List<DocumentsInfo>(); // 5 kategori için dokumanların tutulacağı dokuman lisatesi
         static string[] allCategories = { "ekonomi", "magazin", "saglik", "siyasi", "spor" };
-        static string filePath = @"C:\Users\ozden\Desktop\Yazlab  II\Dokuman Siniflandirma\1150haber\raw_texts\";
+        static string filePath = @"C:\Users\alper\Downloads\1150haber\raw_texts\";
         public static void ReadDirectory()
         {
             for (int i = 0; i < allCategories.Length; i++)
             {
                 string file_path_2 = filePath + allCategories[i];
                 DirectoryInfo directory = new DirectoryInfo(file_path_2);
-                documents.Add( new DocumentsInfo { Files = directory.GetFiles("*.txt"),
+                documents.Add( new DocumentsInfo { files = directory.GetFiles("*.txt"),
                                                    Categori = allCategories[i]
-                                                  });
+                                                 });
             }
         }
         static void Main(string[] args)
