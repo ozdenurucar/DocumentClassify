65f40c7fae4254528e99ef48d31999967b1eeef2
alperenckr <alperenckroglu204@gmail.com>
2018-05-14 17:28:36 +0300

    threading

--- document_classify/document_classify/Program.cs
+++ document_classify/document_classify/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.IO;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace document_classify
 {
@@ -11,6 +12,8 @@ namespace document_classify
     {
         private static readonly string dir = @"C:\Users\alper\Downloads\1150haber\raw_texts\";
         private static List<News> documents = new List<News>();
+        private static Dictionary<string, int> frequencies2 = new Dictionary<string, int>();
+        private static Dictionary<string, int> frequencies3 = new Dictionary<string, int>();
         private static readonly string[] allCategories = { "ekonomi", "magazin", "saglik", "siyasi", "spor" };
         public static void ReadDirectory()
         {
@@ -41,6 +44,43 @@ namespace document_classify
             });
             sw.Stop();
             Console.WriteLine("elapsed time: " + sw.Elapsed.ToString());
+            foreach (var document in documents)
+            {
+                foreach (var gram in document.gram2)
+                {
+                    if (!frequencies2.ContainsKey(gram.Key))
+                        frequencies2.Add(gram.Key, gram.Value);
+                    else
+                        frequencies2[gram.Key] += gram.Value;
+                }
+
+                foreach (var gram in document.gram3)
+                {
+                    if (!frequencies3.ContainsKey(gram.Key))
+                        frequencies3.Add(gram.Key, gram.Value);
+                    else
+                        frequencies3[gram.Key] += gram.Value;
+                }
+            }
+            Thread x = new Thread(() =>
+            {
+                foreach (var iter in frequencies2.Where(kv => kv.Value < 50).ToList())
+                {
+                    frequencies2.Remove(iter.Key);
+                }
+            });
+            x.Start();
+            Thread y = new Thread(() =>
+            {
+                foreach (var iter in frequencies3.Where(kv => kv.Value < 50).ToList())
+                {
+                    frequencies2.Remove(iter.Key);
+                }
+            });
+            y.Start();
+            x.Join();
+            y.Join();
+            Console.WriteLine("----------------------");
             Console.ReadKey();
         }
     }
